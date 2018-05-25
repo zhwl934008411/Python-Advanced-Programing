b7dde2544b37efdc573e6648fe1cdbca8bd0c911
zhao.wenli <a934008411@outlook.com>
2018-05-25 22:55:45 +0800

    update .gitignore

--- index8/xml_pretty.py
+++
@@ -1,9 +0,0 @@
-# coding:utf8
-
-def pretty(e, level=0):
-    if len(e) > 0:
-        e.text = '\n' + '\t' * (level + 1)
-        for child in e:
-            pretty(child, level + 1)
-        child.tail = child.tail[:-1]
-    e.tail = '\n' + '\t' * level
